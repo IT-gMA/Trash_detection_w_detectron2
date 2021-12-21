c83ed75b4d4b46d7e4f642203d2589eb1fd181a9
IT-gMA <tiendatoan@gmail.com>
2021-12-21 12:07:09 +0800

    test function and utils function for test

--- src/test.py
+++ src/test.py
@@ -2,8 +2,15 @@ from detectron2.engine import DefaultPredictor
 import os
 import pickle
 from utils import *
-from configs import CKPT_SAVE_PATH
+from configs import CKPT_SAVE_PATH, TEST_DATA_PATH
 
-with open(CKPT_SAVE_PATH, 'rb') as f:
+'''with open(CKPT_SAVE_PATH, 'rb') as f:
     # Obtain the configuration file
     cfg = pickle.load(f)
+
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # Point to the complete trained model
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7     # Only display detected objects with confidence level greater than 70%
+predictor = DefaultPredictor(cfg)  # Initialiser the object predictor'''
+predictor = None
+
+on_image_batch(TEST_DATA_PATH, predictor)
