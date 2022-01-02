43e60ed80d156fe246118cd53d5fc41a4b162e63
IT-gMA <tiendatoan@gmail.com>
2022-01-02 20:55:21 +0800

    slurm script to start training run from scratch

---
+++ src/run_detectron2_train.sh
@@ -0,0 +1,21 @@
+#!/bin/bash  --login
+
+#SBATCH --partition=gpuq
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:2
+#SBATCH --time=24:00:00
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBTACH --account=interns2021026
+#SBATCH --export=NONE
+#SBATCH --output=mdoan_run.out
+
+module load cuda
+module load singularity
+
+export SINGULARITYENV_PYTHONUSERBASE="/group/interns2021026/mdoan/user_python"
+export SINGULARITYENV_MPLCONFIGDIR="/group/interns2021026/mdoan/user_python/matplotlib"
+export SINGULARITYENV_TORCH_HOME="/group/interns2021026/mdoan/torch_home"
+
+SINGULARITYENV_CUDA=$CUDA_HOME
+time srun singularity exec --nv $MYGROUP/Detectron2_trash_recogntion/detectron2_run_container.sif python $MYGROUP/Detectron2_trash_recogntion/src/train.py
