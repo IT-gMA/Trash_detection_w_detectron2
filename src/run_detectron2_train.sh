28a66a601092024e04c6d1e47080f8f64cb69485
IT-gMA <tiendatoan@gmail.com>
2022-01-05 23:50:33 +0800

    increase run configs

--- src/run_detectron2_train.sh
+++ src/run_detectron2_train.sh
@@ -7,7 +7,7 @@
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBTACH --account=interns2021026
-#SBATCH --output=mdoan_run.out
+#SBATCH --output=mdoan_run2.out
 #SBATCH --export=NONE
 
 module load cuda
@@ -18,4 +18,4 @@ export SINGULARITYENV_MPLCONFIGDIR="/group/interns2021026/mdoan/user_python/matp
 export SINGULARITYENV_TORCH_HOME="/group/interns2021026/mdoan/torch_home"
 
 SINGULARITYENV_CUDA=$CUDA_HOME
-time srun singularity exec --nv $MYGROUP/Detectron2_trash_recogntion/detectron2_run_container.sif python $MYGROUP/Detectron2_trash_recogntion/src/train.py --resume true
+time srun singularity exec --nv $MYGROUP/Detectron2_trash_recogntion/detectron2_run_container.sif python $MYGROUP/Detectron2_trash_recogntion/src/train.py --resume false
