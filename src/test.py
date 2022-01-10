54d3808ac7682940e088f4987ea797d0f9a4a970
IT-gMA <tiendatoan@gmail.com>
2022-01-10 17:32:50 +0800

    slurm script for running infernce on pawsey

--- src/test.py
+++ src/test.py
@@ -1,7 +1,8 @@
 from detectron2.engine import DefaultPredictor
 import pickle
 from utils import *
-from configs import CKPT_SAVE_PATH, INFERENCE_IMG_PATH, TEST_IMG_PATH, ANN_FILE_NAME, TEST_DATASET_NAME
+from configs import CKPT_SAVE_PATH, INFERENCE_IMG_PATH, TEST_IMG_PATH, ANN_FILE_NAME, TEST_DATASET_NAME, \
+    INF_MODEL_PATH, MIN_CONFIDENCE
 from detectron2.data.datasets import register_coco_instances
 import os
 
@@ -16,8 +17,8 @@ register_coco_instances(TEST_DATASET_NAME, {}, TEST_IMG_PATH + "/" + ANN_FILE_NA
 dataset_custom = DatasetCatalog.get(TEST_DATASET_NAME)
 dataset_custom_metadata = MetadataCatalog.get(TEST_DATASET_NAME)
 
-cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # Point to the complete trained model
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.50    # Only display detected objects with confidence level greater than 70%
+cfg.MODEL.WEIGHTS = os.path.join(INF_MODEL_PATH)  # Point to the complete trained model
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = MIN_CONFIDENCE    # Only display detected objects with confidence level greater than 70%
 predictor = DefaultPredictor(cfg)  # Initialise the object predictor
 
 image_inference(INFERENCE_IMG_PATH, predictor, dataset_custom_metadata)
