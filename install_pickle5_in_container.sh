2a4e631e11d0ad52d2a8d714d434b6189f6a8f99
IT-gMA <tiendatoan@gmail.com>
2022-01-10 17:41:01 +0800

    slurm script for running infernce on pawsey

--- install_pickle5_in_container.sh
+++ install_pickle5_in_container.sh
@@ -2,19 +2,17 @@
 
 #SBATCH --partition=gpuq
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:1
+#SBATCH --time=00:45:00
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBTACH --account=interns2021026
-#SBATCH --output=inference_run.out
+#SBATCH --output=log-install-pickle5.out
 #SBATCH --export=NONE
 
-module load cuda
 module load singularity
 
 export SINGULARITYENV_PYTHONUSERBASE="/group/interns2021026/mdoan/user_python"
 export SINGULARITYENV_MPLCONFIGDIR="/group/interns2021026/mdoan/user_python/matplotlib"
 export SINGULARITYENV_TORCH_HOME="/group/interns2021026/mdoan/torch_home"
 
-SINGULARITYENV_CUDA=$CUDA_HOME
-time srun singularity exec --nv $MYGROUP/Detectron2_trash_recogntion/detectron2_run_container.sif python $MYGROUP/Detectron2_trash_recogntion/src/test.py
+time srun singularity exec --nv $MYGROUP/Detectron2_trash_recogntion/detectron2_run_container.sif python3 -m pip install -e pickle5
