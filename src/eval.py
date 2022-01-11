f8b2a1ea5430130f76e6e9a90a76de35c0b01faf
IT-gMA <tiendatoan@gmail.com>
2022-01-11 18:51:13 +0800

    update

--- src/eval.py
+++ src/eval.py
@@ -4,8 +4,10 @@ from detectron2.data.datasets import register_coco_instances
 from detectron2.engine import DefaultPredictor
 import pickle
 from configs import TEST_DATASET_NAME, TEST_IMG_PATH, ANN_FILE_NAME, CKPT_SAVE_PATH, OUTPUT_DIR
+from train import AugTrainer
 
 
+# This is probably to perform evaluation using the test datatset instead of the valid dataset
 def evaluate_model(after_train=False, cfg=None, predictor=None, eval_dataset_name=None):
     if not after_train:
         register_coco_instances(TEST_DATASET_NAME, {}, TEST_IMG_PATH + "/" + ANN_FILE_NAME, TEST_IMG_PATH)
@@ -17,7 +19,8 @@ def evaluate_model(after_train=False, cfg=None, predictor=None, eval_dataset_nam
         eval_dataset_name = TEST_DATASET_NAME
 
     evaluator = COCOEvaluator(eval_dataset_name, cfg, False, output_dir=OUTPUT_DIR)
-    eval_loader = build_detection_test_loader(cfg, eval_dataset_name)
+    #eval_loader = build_detection_test_loader(cfg, eval_dataset_name)
+    eval_loader = AugTrainer.build_test_loader(cfg, eval_dataset_name)
     inference_on_dataset(predictor.model, eval_loader, evaluator)
 
 
